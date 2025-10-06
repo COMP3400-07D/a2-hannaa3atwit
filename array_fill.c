690d53cad24402d65824cc19e5fab46025de5fb2
hannaa3atwit <hannaa3@wit.edu>
2025-10-06 10:59:22 -0400

    finished array_fill

--- array_fill.c
+++ array_fill.c
@@ -1,8 +1,19 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * fills array with increasing numbers from begin to end
+ * stops when either the end value is reached or the array is full
+ * Returns the number of elements actually filled
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    if (array == NULL || array_len <= 0) {
+        return 0; // no elements filled
+    }
+
+    int count = 0;
+    for (int i = begin; i <= end && count < array_len; i++) {
+        array[count++] = i;
+    }
+
+    return count; // number of elements filled
 }
